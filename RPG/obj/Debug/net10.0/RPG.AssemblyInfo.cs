5d352af3236999c03ebc9fcf62ba82248fd4e195
Dipper22Dip <ivan.lebedev.2002g@mail.ru>
2026-04-12 07:42:52 +0700

    Delete the DataJson dir from workSpace.

--- RPG/obj/Debug/net10.0/RPG.AssemblyInfo.cs
+++ RPG/obj/Debug/net10.0/RPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3c027805795b78f7f7161941b050e4dd9cf5107")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c15b43c45f520665aab62f83bcfa69bcad1a4256")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
